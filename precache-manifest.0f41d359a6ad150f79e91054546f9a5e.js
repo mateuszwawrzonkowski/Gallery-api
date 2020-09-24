68547bcb8d741c1b053d8c86c57cffde69a452d7
mwawrzonkowski <wawko2@gmail.com>
2020-09-24 23:31:20 +0200

    Updates

---
+++ precache-manifest.0f41d359a6ad150f79e91054546f9a5e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "957046bee6f058b3d059dc9c725fe572",
+    "url": "/task-higher/index.html"
+  },
+  {
+    "revision": "c452b1f22e47886a4f7a",
+    "url": "/task-higher/static/js/2.6dfbd4ff.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/task-higher/static/js/2.6dfbd4ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6af64be6d210684048d1",
+    "url": "/task-higher/static/js/main.2c48fe82.chunk.js"
+  },
+  {
+    "revision": "4bed3a0e4ba5b0ce0ff6",
+    "url": "/task-higher/static/js/runtime-main.be374268.js"
+  }
+]);
